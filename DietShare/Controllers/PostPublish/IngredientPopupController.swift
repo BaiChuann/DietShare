787330d47ee791268f886ea3bd9012fdbedee186
Fan Weiguang <lahalaha90@gmail.com>
2018-03-23 16:01:23 +0800

    Update ingredient popup input ui

--- DietShare/Controllers/PostPublish/IngredientPopupController.swift
+++ DietShare/Controllers/PostPublish/IngredientPopupController.swift
@@ -11,6 +11,7 @@ import UIKit
 class IngredientPopupController: UIViewController {
     @IBOutlet private var inputGroup: [UITextField]!
     @IBOutlet private var unitButtonGroup: [UIButton]!
+    @IBOutlet weak private var saveButton: UIButton!
 
     private var currentSelectedUnit = 0
     private let normalColor = hexToUIColor(hex: "#9CA0A1")
@@ -19,25 +20,38 @@ class IngredientPopupController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setUpUnitButton()
+        setUpUI()
         setUpInputDelegate()
         addInputBorder(for: inputGroup, withColor: hexToUIColor(hex: "#CACFD0"))
 
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(endEditing)))
     }
 
-    @IBAction func onUnitSelected(_ sender: UIButton!) {
-        sender.tintColor = highlightColor
-        sender.isSelected = true
+    @IBAction func onUnitSelected(_ sender: Any) {
+        if let button = sender as? UIButton {
+            button.tintColor = highlightColor
+            button.isSelected = true
 
-        let previouslySelected = unitButtonGroup.first { $0.tag == currentSelectedUnit }
-        previouslySelected?.isSelected = false
-        previouslySelected?.tintColor = normalColor
+            let previouslySelected = unitButtonGroup.first { $0.tag == currentSelectedUnit }
+            previouslySelected?.isSelected = false
+            previouslySelected?.tintColor = normalColor
 
-        currentSelectedUnit = sender.tag
+            currentSelectedUnit = button.tag
+        }
     }
 
-    func setUpUnitButton() {
+    @IBAction func onSaveButtonPressed(_ sender: Any) {
+    }
+
+    func setUpUI() {
+        inputGroup.forEach {
+            $0.placeholder = nil
+
+            let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: $0.frame.height))
+            $0.leftView = paddingView
+            $0.leftViewMode = .always
+        }
+
         unitButtonGroup.forEach {
             let tintedImage = $0.currentImage?.withRenderingMode(.alwaysTemplate)
 
@@ -47,8 +61,13 @@ class IngredientPopupController: UIViewController {
 
             if $0.tag == currentSelectedUnit {
                 $0.isSelected = true
+                $0.tintColor = highlightColor
             }
         }
+
+        saveButton.layer.cornerRadius = 10
+        saveButton.backgroundColor = highlightColor
+        saveButton.setTitleColor(UIColor.white, for: .normal)
     }
 
     func setUpInputDelegate() {
@@ -67,3 +86,4 @@ extension IngredientPopupController: UITextFieldDelegate {
         return true
     }
 }
+
