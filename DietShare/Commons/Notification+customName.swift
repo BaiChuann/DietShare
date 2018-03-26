30a07082461a1e541a61799478026da44427af1e
Shuang Yang <david.yangshuang@icloud.com>
2018-03-26 15:23:43 +0800

    Add Topic model

--- DietShare/Commons/Notification+customName.swift
+++ DietShare/Commons/Notification+customName.swift
@@ -5,6 +5,7 @@
 //  Created by Shuang Yang on 26/3/18.
 //  Copyright © 2018 com.marvericks. All rights reserved.
 //
+import Foundation
 
 extension Notification.Name {
     static let didUpdateRestaurantFilter = Notification.Name("didUpdateRestaurantFilter")
