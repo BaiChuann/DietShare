332a7240326f50e270f15510eb76e3e74d97aa7c
Shuang Yang <david.yangshuang@icloud.com>
2018-03-26 14:24:07 +0800

    Add Common files

---
+++ DietShare/Commons/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  DietShare
+//
+//  Created by Shuang Yang on 26/3/18.
+//  Copyright © 2018 com.marvericks. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+}
---
+++ DietShare/Commons/Notification+customName.swift
@@ -0,0 +1,11 @@
+//
+//  Notification+customName.swift
+//  DietShare
+//
+//  Created by Shuang Yang on 26/3/18.
+//  Copyright © 2018 com.marvericks. All rights reserved.
+//
+
+extension Notification.Name {
+    static let didUpdateRestaurantFilter = Notification.Name("didUpdateRestaurantFilter")
+}
