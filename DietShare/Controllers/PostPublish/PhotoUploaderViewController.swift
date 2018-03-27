c7ef43d10868ec3396a23f1bf13da75b01aeab52
ZiyangMou <hflsmzy@gmail.com>
2018-03-27 22:30:16 +0800

    Add TGCameraController

--- DietShare/Controllers/PostPublish/PhotoUploaderViewController.swift
+++ DietShare/Controllers/PostPublish/PhotoUploaderViewController.swift
@@ -5,36 +5,80 @@
 //  Created by ZiyangMou on 24/3/18.
 //  Copyright © 2018 com.marvericks. All rights reserved.
 //
+// swiftlint:disable implicitly_unwrapped_optional
+// swiftlint:disable force_unwrapping
 
 import Foundation
 import UIKit
+import TGCameraViewController
 
-class PhotoUploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    @IBOutlet private var imagePicked: UIImageView!
-    let imagePicker = UIImagePickerController()
+class PhotoUploadViewController: UIViewController, TGCameraDelegate {
+    var pickedPhoto: UIImage?
+    private var isToCamera: Bool = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        imagePicker.delegate = self
-        CameraHandler.shared.showActionSheet(viewController: self)
-        CameraHandler.shared.imagePickedBlock = { image in
-           self.imagePicked.image = image
-        }
-        /*let cameraViewController = CameraViewController { [weak self] image, asset in
-            self?.imagePicked.image = image
-            self?.dismiss(animated: true, completion: nil)
-        }
-        present(cameraViewController, animated: true, completion: nil)*/
-    }
-}
+        // Use yellow tint
+        TGCameraColor.setTint(.yellow)
 
-extension PhotoUploadViewController {
-    @IBAction func openCameraButton(sender: AnyObject) {
-        CameraHandler.shared.camera()
+        // save image to album
+        TGCamera.setOption(kTGCameraOptionSaveImageToAlbum, value: true)
+
+        // use the original image aspect instead of square
+        //TGCamera.setOption(kTGCameraOptionUseOriginalAspect, value: true)
+
+        // hide filter button
+        TGCamera.setOption(kTGCameraOptionHiddenFilterButton, value: true)
     }
 
-    @IBAction func openPhotoLibraryButton(sender: AnyObject) {
-        CameraHandler.shared.photoLibrary()
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        switch isToCamera {
+        case true:
+            openCamera()
+        case false:
+            goBackToDiscovery()
+        }
+    }
+
+    func cameraDidCancel() {
+        dismiss(animated: true, completion: nil)
+    }
+
+    func cameraDidSelectAlbumPhoto(_ image: UIImage!) {
+        pickedPhoto = image
+        dismiss(animated: true, completion: nil)
+    }
+
+    func cameraDidTakePhoto(_ image: UIImage!) {
+        pickedPhoto = image
+        dismiss(animated: true, completion: nil)
+    }
+
+    // Optional
+
+    func cameraWillTakePhoto() {
+        print("cameraWillTakePhoto")
+    }
+
+    func cameraDidSavePhoto(atPath assetURL: URL!) {
+        print("cameraDidSavePhotoAtPath: \(assetURL)")
+    }
+
+    func cameraDidSavePhotoWithError(_ error: Error!) {
+        print("cameraDidSavePhotoWithError \(error)")
+    }
+
+    private func openCamera() {
+        let navigationController = TGCameraNavigationController.new(with: self)
+        isToCamera = false
+        present(navigationController!, animated: true, completion: nil)
+    }
+
+    private func goBackToDiscovery() {
+        isToCamera = true
+        tabBarController?.selectedIndex = 1
     }
 }
