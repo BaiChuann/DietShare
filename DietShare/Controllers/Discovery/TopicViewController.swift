893324d729a440033699257f3186c6a60d2cae4f
Shuang Yang <david.yangshuang@icloud.com>
2018-03-28 19:57:43 +0800

    Modify Topic View

--- DietShare/Controllers/Discovery/TopicViewController.swift
+++ DietShare/Controllers/Discovery/TopicViewController.swift
@@ -2,35 +2,19 @@
 //  TopicViewController.swift
 //  DietShare
 //
-//  Created by Shuang Yang on 27/3/18.
+//  Created by Shuang Yang on 28/3/18.
 //  Copyright © 2018 com.marvericks. All rights reserved.
 //
 
 import UIKit
 
-class TopicViewController<T: ReadOnlyTopic> : UIViewController {
+class TopicViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
-    private var topic = Topic()
-    
-//    @IBOutlet weak var topicName: UILabel!
-//    @IBOutlet weak var topicDescription: UITextView!
-//    @IBOutlet weak var topicImage: UIImageView!
+    @IBOutlet weak var topicImage: UIImageView!
+    @IBOutlet weak var topicDescription: UITextView!
+    @IBOutlet weak var followButton: UIButton!
     
     @IBOutlet weak var activeUsers: UICollectionView!
-
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if collectionView == activeUsers {
-            return Constants.TopicPage.numOfDisplayedUsers
-        }
-        return 0
-    }
-
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Identifiers.activeUserListCell, for: indexPath as IndexPath) as! ActiveUserListCell
-        return cell
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -41,5 +25,13 @@ class TopicViewController<T: ReadOnlyTopic> : UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return Constants.TopicPage.numOfDisplayedUsers
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Identifiers.activeUserListCell, for: indexPath as! IndexPath) as! ActiveUserListCell
+        return cell
+    }
 }
