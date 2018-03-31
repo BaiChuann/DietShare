a00c25ccdc1ea362c7042e4ed6cc7d0be997aed3
Fan Weiguang <lahalaha90@gmail.com>
2018-04-01 02:12:06 +0800

    update layout for photo preview

--- DietShare/Controllers/PostPublish/LayoutPhotoSelectorController.swift
+++ DietShare/Controllers/PostPublish/LayoutPhotoSelectorController.swift
@@ -16,45 +16,66 @@ class LayoutPhotoSelectorController: UIViewController {
     @IBOutlet weak private var previewCollectionView: UICollectionView!
 
     private let pickerController = DKImagePickerController()
-    private let spacing = CGFloat(5)
+    private let minNumberOfImages = 2
     private let maxNumberOfImages = 4
     private let previewCellIdentifier = "LayoutPhotoPreviewCell"
-    private var selectedAssets = [DKAsset]()
+    private var selectedImages = [UIImage]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        enableNextButton(shouldEnable: false)
         setUpUI()
-        setUpPicker()
     }
 
-    private func setUpUI() {
-        nextButton.layer.cornerRadius = Constants.cornerRadius
-    }
-
-    private func setUpPicker() {
-        pickerController.inline = true
-        pickerController.sourceType = .photo
-        pickerController.assetType = .allPhotos
-        pickerController.UIDelegate = LayoutPhotoSelectorUIDelegate()
-        pickerController.maxSelectableCount = maxNumberOfImages
-
+    override func viewWillLayoutSubviews() {
         if let pickerView = pickerController.view {
-            let startingY = backButton.frame.origin.y + backButton.bounds.height + spacing
+            let spacing: CGFloat = 5
+            let startingY = backButton.frame.maxY + spacing
             let height = preview.frame.origin.y - startingY
             pickerView.frame = CGRect(x: 0, y: startingY, width: view.bounds.width, height: height)
+
             pickerView.backgroundColor = Constants.photoLibraryBackgroundColor
             view.addSubview(pickerView)
         }
 
         pickerController.selectedChanged = { [unowned self] in
-            print("selected changed")
             self.updateSelection()
         }
     }
 
+    private func setUpUI() {
+        nextButton.layer.cornerRadius = Constants.cornerRadius
+        pickerController.inline = true
+        pickerController.sourceType = .photo
+        pickerController.assetType = .allPhotos
+        pickerController.UIDelegate = LayoutPhotoSelectorUIDelegate()
+        pickerController.maxSelectableCount = maxNumberOfImages
+    }
+
     private func updateSelection() {
-        selectedAssets = pickerController.selectedAssets
+        var images = [UIImage]()
+        pickerController.selectedAssets.forEach {
+            $0.fetchOriginalImage(true) { image, _ in
+                if let image = image {
+                    images.append(image)
+                }
+            }
+        }
+        selectedImages = images
+
+        enableNextButton(shouldEnable: selectedImages.count > minNumberOfImages)
         previewCollectionView.reloadData()
+    }
+
+    private func enableNextButton(shouldEnable: Bool) {
+        nextButton.isEnabled = shouldEnable
+
+        if shouldEnable {
+            nextButton.alpha = 1
+        } else {
+            nextButton.alpha = 0.5
+        }
     }
 
     @IBAction func onBackButtonPressed(_ sender: Any) {
@@ -74,12 +95,8 @@ extension LayoutPhotoSelectorController: UICollectionViewDelegate, UICollectionV
             return cell
         }
 
-        let asset = selectedAssets[indexPath.item]
-        asset.fetchOriginalImage(true) { image, _ in
-            if let image = image {
-                previewCell.setImage(image)
-            }
-        }
+        let image = selectedImages[indexPath.item]
+        previewCell.setImage(image)
 
         return previewCell
     }
@@ -94,6 +111,6 @@ extension LayoutPhotoSelectorController: UICollectionViewDelegate, UICollectionV
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return selectedAssets.count
+        return selectedImages.count
     }
 }
