003db1f7fd487236263d82b0c55100bda0162241
Fan Weiguang <lahalaha90@gmail.com>
2018-04-11 21:48:25 +0800

    update layout for floating text adder

--- DietShare/Views/Share/TextColorCell.swift
+++ DietShare/Views/Share/TextColorCell.swift
@@ -11,31 +11,41 @@ import UIKit
 class TextColorCell: UICollectionViewCell {
     @IBOutlet weak private var colorView: TextColorCell!
 
-    private let inset: CGFloat = 8
+    private let inset: CGFloat = 5
 
-    override var isSelected: Bool {
-        didSet {
-            var newFrame = CGRect.zero
-            if super.isSelected {
-                newFrame = CGRect(x: 0, y: 0, width: contentView.bounds.width, height: contentView.bounds.height)
-            } else {
-                newFrame = CGRect(x: inset, y: inset, width: contentView.bounds.width - 2 * inset, height: contentView.bounds.height - 2 * inset)
-            }
-
-            UIView.animate(withDuration: 0.3) {
-                self.colorView.frame = newFrame
-                self.colorView.layer.cornerRadius = newFrame.width / 2
-            }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        if colorView != nil {
+            colorView.layer.cornerRadius = Constants.cornerRadius
         }
     }
 
     func setColor(_ color: UIColor) {
-        colorView.layer.cornerRadius = colorView.frame.width / 2
         colorView.backgroundColor = color
 
         if color.equals(UIColor.white) {
             colorView.layer.borderColor = Constants.lightBackgroundColor.cgColor
             colorView.layer.borderWidth = 2
         }
+    }
+
+    func setSelected(_ isSelected: Bool) {
+        if isSelected == self.isSelected {
+            return
+        }
+
+        var newFrame = CGRect.zero
+        if isSelected {
+            newFrame = CGRect(x: 0, y: 0, width: contentView.bounds.width, height: contentView.bounds.height)
+        } else {
+            newFrame = CGRect(x: inset, y: inset, width: contentView.bounds.width - 2 * inset, height: contentView.bounds.height - 2 * inset)
+        }
+
+        UIView.animate(withDuration: 0.3) {
+            self.colorView.frame = newFrame
+        }
+
+        self.isSelected = isSelected
     }
 }
